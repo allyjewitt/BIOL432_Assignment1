1ed75dd595ebd8216df6a03313023260ada71aaf
Ally Jewitt <122380677+allyjewitt@users.noreply.github.com>
2023-01-10 14:00:57 -0500

    Add files via upload
    Adding script for limb volume

---
+++ volumeEstimato.R
@@ -0,0 +1,8 @@
+Import
+MyData<-read.csv("measurements.csv", header=T)
+#Load the dplyr package
+library(dplyr)
+#Add a new column that calculates volume using the equation for volume of a cylinder πr^2h
+MyData<-MyData %>% 
+  mutate(Volume=pi*((MyData$Limb_width/2)^2)*MyData$Limb_length)
+write.csv(MyData, "measurements.csv", row.names=TRUE)  
